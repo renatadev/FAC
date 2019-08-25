412dd0b6802f284ae8ab32e28b21b9053c167def
Renata <[renatajarmova@gmail.com]>
2019-08-25 10:20:32 +0100

    Add feature to change carousel slides with JS

--- js/script.js
+++ js/script.js
@@ -80,3 +80,29 @@ function setupTypewriter(t) {
 var typer = document.getElementById('typewriter');
 typewriter = setupTypewriter(typewriter);
 typewriter.type();
+
+// ----- Carousel -----//
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
